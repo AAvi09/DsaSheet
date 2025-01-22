6caa0aa04dcb6ac4854ece59b0692ba0edea43e5
Avinash sharma <sharmaavi2196@gmail.com>
2025-01-22 21:04:30 +0530

    created append and prepend function to add nodes at the head and tail of the linked list

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log("enter the dragon");
+const myLinkedList = {
+  value: 10,
+  next: {
+    value: 5,
+    next: {
+      value: 16,
+      next: null,
+    },
+  },
+};
+
+console.log(myLinkedList);
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: 10,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    (this.tail.next = newNode), (this.tail = newNode), this.length++;
+    return this;
+  }
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: this.head,
+    };
+    this.head = newNode;
+    this.length++;
+  }
+}
+
+const brandNewLinkedList = new LinkedList(10);
+brandNewLinkedList.append(5);
+brandNewLinkedList.append(12);
+brandNewLinkedList.prepend(20);
+console.log(brandNewLinkedList);
